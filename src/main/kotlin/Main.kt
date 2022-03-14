917fe59db56ff112bf45dc6113209117e9abc003
Andrei <gagarin1982@gmail.com>
2022-03-14 12:18:32 +0500

    Ready

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,7 @@ enum class AccountType {
 
 fun main() {
     printMenu()
-    val accountTypeNum = readln().toInt()
-    val accountType: AccountType = when (accountTypeNum) {
+    val accountType: AccountType = when (readln().toInt()) {
         1 -> AccountType.Maestro
         2 -> AccountType.Mastercard
         3 -> AccountType.Visa
@@ -23,27 +22,30 @@ fun main() {
     print("Enter transfer amount -> ")
     val transferAmount = readln().toInt()
 
-    commissionCalculation(transferAmount, accountType, lastTransferAmount)
+    println(
+        "Transfer commission: ${
+            if (isLimit(transferAmount, accountType, lastTransferAmount)) commissionCalculation(
+                transferAmount,
+                accountType
+            ) else "Limit is exceeded"
+        }"
+    )
 }
 
 fun commissionCalculation(
     transferAmount: Int,
     accountType: AccountType = AccountType.VKPay,
-    lastTransferAmount: Int = 0
-) {
-    if ((accountType == AccountType.VKPay && (lastTransferAmount <= VK_MONTH_LIMIT || transferAmount <= VK_LIMIT)) ||
-        (accountType != AccountType.VKPay && (lastTransferAmount <= MONTH_LIMIT || transferAmount <= DAY_LIMIT))
-    ) {
-        val visaMirCommission = (transferAmount * .75 / 100).toInt()
-        val calculatedCommission: Int = when (accountType) {
-            AccountType.Maestro, AccountType.Mastercard ->
-                (if (transferAmount in 30_000..7_500_000) 0 else transferAmount * .6 / 100 + 20).toInt()
-            AccountType.Visa, AccountType.Mir ->
-                if (visaMirCommission <= 3_500) 3_500 else visaMirCommission
-            else -> 0
-        }
-        println("Transfer commission: $calculatedCommission")
-    } else println("Limit is exceeded")
+): Int {
+    val calculatedCommission: Int
+    val visaMirCommission = (transferAmount * .75 / 100).toInt()
+    calculatedCommission = when (accountType) {
+        AccountType.Maestro, AccountType.Mastercard ->
+            (if (transferAmount in 30_000..7_500_000) 0 else transferAmount * .6 / 100 + 20).toInt()
+        AccountType.Visa, AccountType.Mir ->
+            if (visaMirCommission <= 3_500) 3_500 else visaMirCommission
+        else -> 0
+    }
+    return calculatedCommission
 }
 
 fun printMenu() {
@@ -57,4 +59,13 @@ fun printMenu() {
         | -> 
     """.trimMargin()
     )
+}
+
+fun isLimit(
+    transferAmount: Int,
+    accountType: AccountType = AccountType.VKPay,
+    lastTransferAmount: Int = 0
+): Boolean {
+    return (accountType == AccountType.VKPay && (lastTransferAmount <= VK_MONTH_LIMIT || transferAmount <= VK_LIMIT)) ||
+            (accountType != AccountType.VKPay && (lastTransferAmount <= MONTH_LIMIT || transferAmount <= DAY_LIMIT))
 }
